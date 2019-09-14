831c183e280f71618434c160de66abd5b3025cf0
J.Farrow <jack.dempsey.farrow@gmail.com>
2019-09-14 14:43:17 -0400

    Ejects and creates initial project structure

---
+++ src/components/Content/Content.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Content extends Component{
+    render(){
+        return(
+            <main>
+                <h2>Content</h2>
+            </main>
+        );
+    }
+}
